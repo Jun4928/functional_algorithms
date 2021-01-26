a441f16856389e798d2583eb38e35d4c2e4cbc30
Jun4928 <jun.choi.4928@gmail.com>
2021-01-26 13:28:34 +0900

    Add: 2021_kakao_new_id

---
+++ 2021_kakao_new_id.js
@@ -0,0 +1,39 @@
+const log = console.log
+const { go, curry, take } = require('./functions')
+
+const ALLOWED = /[^-_.a-z0-9]/g
+const DOTS_MORE_THAN_TWO = /\.{2,}/g
+const FIRST_AND_LAST_DOT = /^\.|\.$/g
+
+const toLowerCase = (str) => str.toLowerCase()
+const join = (arr) => arr.join('')
+const replace = curry(({ parser, replacement }, str) => str.replace(parser, replacement))
+const ifEmpty = (str) => (str.length ? str : 'a')
+const isLonger = (str) => (str.length >= 16 ? go(str, take(15), join) : str)
+const repeatLast = curry((limit, str) => {
+  const repeat = (str) => {
+    if (str.length >= limit) return str
+    else return repeat(str.concat('', str[str.length - 1]))
+  }
+
+  return repeat(str)
+})
+
+const solution = (newId) =>
+  go(
+    newId,
+    toLowerCase,
+    replace({ parser: ALLOWED, replacement: '' }),
+    replace({ parser: DOTS_MORE_THAN_TWO, replacement: '.' }),
+    replace({ parser: FIRST_AND_LAST_DOT, replacement: '' }),
+    ifEmpty,
+    isLonger,
+    replace({ parser: FIRST_AND_LAST_DOT, replacement: '' }),
+    repeatLast(3)
+  )
+
+log(solution('...!@BaT#*..y.abcdefghijklm'))
+log(solution('z-+.^.'))
+log(solution('=.='))
+log(solution('123_.def'))
+log(solution('abcdefghijklmn.p'))
